4456e7be255f59528080f538dc5414cb7295c2ed
Juli-GR <100521477+Juli-GR@users.noreply.github.com>
2022-08-29 17:20:38 -0300

    Update Proyecto1.hs

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -49,21 +49,22 @@ pertenece a (x:xs)  | (a == x) = True
 
 --EJERCICIO 4-----------------------------------------------
 
-paratodo' :: [a] -> (a -> Bool) -> Bool
-paratodo' [] f = True
-paratodo' (x:xs) f = (f x) && (paratodo' xs f)
 
-existe' :: [a] -> (a -> Bool) -> Bool
-existe' [] f = False
-existe' (x:xs) f = (f x) || (existe' xs f)
+--paratodo' :: [a] -> (a -> Bool) -> Bool
+--paratodo' [] f = True
+--paratodo' (x:xs) f = (f x) && (paratodo' xs f)
 
-sumatoria' :: [a] -> (a -> Int) -> Int
-sumatoria' [] f = 0
-sumatoria' (x:xs) f = (f x) + (sumatoria' xs f)
+--existe' :: [a] -> (a -> Bool) -> Bool
+--existe' [] f = False
+--existe' (x:xs) f = (f x) || (existe' xs f)
 
-productoria' :: [a] -> (a -> Int) -> Int
-productoria' [] f = 1
-productoria' (x:xs) f = (f x) * (productoria' xs f)
+--sumatoria' :: [a] -> (a -> Int) -> Int
+--sumatoria' [] f = 0
+--sumatoria' (x:xs) f = (f x) + (sumatoria' xs f)
+
+--productoria' :: [a] -> (a -> Int) -> Int
+--productoria' [] f = 1
+--productoria' (x:xs) f = (f x) * (productoria' xs f)
 
 
 
@@ -100,3 +101,92 @@ multiplicaPares xs = productoria (filter even xs)   --usar primera fun productor
 
 --filter a partir de una lista, devuelve otra solo con los elementos que cumplen cierta condición
 --filter esPositivo [1, -4, 6, 2, -8] equivale a [1, 6, 2]
+
+
+
+
+
+duplica :: [Int] -> [Int]
+duplica [] = []
+duplica (x:xs) = (2*x) : duplica xs  
+
+
+duplica' :: [Int] -> [Int]
+duplica' xs = map (*2) xs
+
+sonpares :: [Int] -> [Int]
+sonpares [] = []
+sonpares (x:xs) | mod x 2 == 0 = x : sonpares xs
+                | otherwise = sonpares xs 
+                
+                
+
+sonpares' :: [Int] -> [Int]
+sonpares' xs = filter even xs
+
+
+primIgualesA :: Eq (a) => a -> [a] -> [a]
+primIgualesA a [] = []
+primIgualesA a (x:xs) | (a == x) = (x : (primIgualesA x xs))
+                      | otherwise = []
+                
+      
+      
+
+primIgualesA' ::  Eq (a) => a -> [a] -> [a]  
+primIgualesA' a xs = takeWhile (== a) xs
+
+
+
+--primIguales :: Eq (a) => [a] -> [a]
+--primIguales [] = []
+--primIguales (x:[]) = x : primIguales []
+--primIguales (x:y:xs)  x == y = x : primIguales (y:xs)
+
+               
+                      
+primIguales :: Eq (a) => [a] -> [a]
+primIguales [] = []
+primIguales (x:xs) | (xs!!0 == x) = x : primIguales xs 
+                   | otherwise = [x]
+                   
+                   
+
+primIguales' :: Eq (a) => [a] -> [a]
+primIguales' xs = primIgualesA' (xs!!0) xs
+
+
+
+
+cuantGen :: (b -> b -> b) -> b -> [a] -> (a -> b) -> b
+cuantGen op z [] t = z
+cuantGen op z (x:xs) t = op (t x) (cuantGen op z xs t)
+
+
+
+paratodo' :: [a] -> (a -> Bool) -> Bool
+paratodo' xs f = cuantGen (&&) (True) xs f 
+
+existe' :: [a] -> (a -> Bool) -> Bool
+existe' xs f = cuantGen (||) (False) xs f 
+
+sumatoria' :: [a] -> (a -> Int) -> Int
+sumatoria' xs f = cuantGen (+) (0) xs f 
+
+productoria' :: [a] -> (a -> Int) -> Int
+productoria' xs f = cuantGen (*) (1) xs f 
+
+
+--13
+--a, bien
+--b, mal, ((a,b):xs)
+--c, bien
+--d, non exhaustive patterns
+--e, non exhaustive patterns
+--f
+
+f :: [(Int,a)] -> Int
+f [(0,a)] = 3
+
+--14
+--aparte de la a, ninguna se puede porq no existen funciones q tienen cualquier input y el output es cualquiera pero distinto
