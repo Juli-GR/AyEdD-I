9cac2b7f3dda4988fc6da157e4dcfc19244058af
Juli-GR <100521477+Juli-GR@users.noreply.github.com>
2022-08-29 21:54:20 -0300

    Update Proyecto1.hs

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -31,10 +31,12 @@ productoria (x:xs) = x * (productoria xs)
 factorial :: Int -> Int
 factorial n | (n == 1) = 1
             | (n > 1) = n * (factorial (n-1))
-            | (n < 1) = 0                         --Si ingresa neg, dejo q de error?
 
 promedio :: [Int] -> Int
-promedio xs = div (sumatoria xs) (length xs)        --Podemos usar div? como hago div entera si no?
+promedio xs = div (sumatoria xs) (length xs)
+--Usé div porque hace división entera, a diferencia de "/"
+--Tambien se puede hacer manualmente con una función
+--que antes le reste a la sumatoria el sobrante usando modulo lenght xs
 
 
 
@@ -78,13 +80,17 @@ paratodo xs f = paratodo' xs f                  --Era esta la idea? medio inutil
 --EJERCICIO 6-----------------------------------------------
 
 todosPares :: [Int] -> Bool
-todosPares xs = paratodo' xs (even)     --even? con mod o div no funciona??
+todosPares xs = paratodo' xs (even)
+--Se usó even, pero podría haberse definido una función por casos con módulo 2
 
 hayMultiplo :: Int -> [Int] -> Bool
-hayMultiplo n xs = existe' (map (`mod` n) xs) (==0)     --Preguntar!!
+hayMultiplo n xs = existe' (map (`mod` n) xs) (==0)
+--Se usó `mod` que invierte las variables,
+--pero se podría haber definido una función para ello.
 
 sumaCuadrados :: Int -> Int
-sumaCuadrados n = sumatoria' [0..n-1] (^2)  -- usar ^ ?? (el rango esta bien)
+sumaCuadrados n = sumatoria' [0..n-1] (^2)
+--Se usó ^, y no se por que xd
 
 factorial' :: Int -> Int    --Usa recursion pero no justo acá(?)
 factorial' n = productoria [1..n]
@@ -104,64 +110,63 @@ multiplicaPares xs = productoria (filter even xs)   --usar primera fun productor
 
 
 
-
+--EJERCICIO 8-----------------------------------------------
 
 duplica :: [Int] -> [Int]
 duplica [] = []
-duplica (x:xs) = (2*x) : duplica xs  
-
+duplica (x:xs) = (2*x) : duplica xs
 
 duplica' :: [Int] -> [Int]
 duplica' xs = map (*2) xs
+
+
+
+--EJERCICIO 9-----------------------------------------------
 
 sonpares :: [Int] -> [Int]
 sonpares [] = []
 sonpares (x:xs) | mod x 2 == 0 = x : sonpares xs
                 | otherwise = sonpares xs 
-                
-                
 
 sonpares' :: [Int] -> [Int]
 sonpares' xs = filter even xs
 
 
+
+--EJERCICIO 10----------------------------------------------
+
 primIgualesA :: Eq (a) => a -> [a] -> [a]
 primIgualesA a [] = []
 primIgualesA a (x:xs) | (a == x) = (x : (primIgualesA x xs))
                       | otherwise = []
-                
-      
-      
 
 primIgualesA' ::  Eq (a) => a -> [a] -> [a]  
 primIgualesA' a xs = takeWhile (== a) xs
 
 
 
+--EJERCICIO 11----------------------------------------------
+
 --primIguales :: Eq (a) => [a] -> [a]
 --primIguales [] = []
 --primIguales (x:[]) = x : primIguales []
 --primIguales (x:y:xs)  x == y = x : primIguales (y:xs)
 
-               
-                      
 primIguales :: Eq (a) => [a] -> [a]
 primIguales [] = []
 primIguales (x:xs) | (xs!!0 == x) = x : primIguales xs 
                    | otherwise = [x]
-                   
-                   
 
 primIguales' :: Eq (a) => [a] -> [a]
 primIguales' xs = primIgualesA' (xs!!0) xs
 
 
 
+--EJERCICIO 12----------------------------------------------
 
 cuantGen :: (b -> b -> b) -> b -> [a] -> (a -> b) -> b
 cuantGen op z [] t = z
 cuantGen op z (x:xs) t = op (t x) (cuantGen op z xs t)
-
 
 
 paratodo' :: [a] -> (a -> Bool) -> Bool
@@ -177,7 +182,9 @@ productoria' :: [a] -> (a -> Int) -> Int
 productoria' xs f = cuantGen (*) (1) xs f 
 
 
---13
+
+--EJERCICIO 13----------------------------------------------
+
 --a, bien
 --b, mal, ((a,b):xs)
 --c, bien
@@ -188,5 +195,7 @@ productoria' xs f = cuantGen (*) (1) xs f
 f :: [(Int,a)] -> Int
 f [(0,a)] = 3
 
---14
+
+
+--EJERCICIO 14----------------------------------------------
 --aparte de la a, ninguna se puede porq no existen funciones q tienen cualquier input y el output es cualquiera pero distinto
