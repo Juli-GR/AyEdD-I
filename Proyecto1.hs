7051416ad0f85132773aef65d3536906ca0af424
Juli-GR <100521477+Juli-GR@users.noreply.github.com>
2022-09-04 13:48:04 -0300

    Update Proyecto1.hs

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -40,9 +40,9 @@ productoria (x:xs) = x * (productoria xs)
 
 promedio :: [Int] -> Int
 promedio xs = div (sumatoria xs) (length xs)
---Usé div porque hace división entera, a diferencia de "/"
+--Se usó div porque hace división entera, a diferencia de "/"
 --Tambien se puede hacer manualmente con una función
---que antes le reste a la sumatoria el sobrante usando modulo lenght xs
+--que antes le reste a la sumatoria el sobrante usando modulo lenght xs.
 
 
 
@@ -87,12 +87,12 @@ paratodo xs = paratodo' xs id
 
 todosPares :: [Int] -> Bool
 todosPares xs = paratodo' xs (even)
---Se usó even, pero podría haberse definido una función por casos con módulo 2
+--Se usó even, pero podría haberse definido una función por casos con módulo 2.
 
 hayMultiplo :: Int -> [Int] -> Bool
 hayMultiplo n xs = existe' (map (`mod` n) xs) (==0)
 --Se usó `mod` que invierte las variables,
---pero se podría haber definido una función para ello
+--pero se podría haber definido una función para ello.
 
 sumaCuadrados :: Int -> Int
 sumaCuadrados n = sumatoria' [0..n-1] (^2)
@@ -107,11 +107,11 @@ factorial n = productoria [1..n]
 
 --EJERCICIO 7-----------------------------------------------
 
---map aplica una función a cada elemento de una lista, y devuelve la nueva lista
---map succ [1, -4, 6, 2, -8] equivale a [2, -3, 7, 3, -7]
+--map aplica una función a cada elemento de una lista, y devuelve la nueva lista.
+--map succ [1, -4, 6, 2, -8] equivale a [2, -3, 7, 3, -7].
 
---filter a partir de una lista, devuelve otra solo con los elementos que cumplen cierta condición
---filter esPositivo [1, -4, 6, 2, -8] equivale a [1, 6, 2]
+--filter a partir de una lista, devuelve otra solo con los elementos que cumplen cierta condición.
+--filter esPositivo [1, -4, 6, 2, -8] equivale a [1, 6, 2].
 
 
 
@@ -185,26 +185,36 @@ productoria' xs f = cuantGen (*) (1) xs f
 
 
 
---EJERCICIO 13---------------------------------------------- FALTA
+--EJERCICIO 13----------------------------------------------
 
---a, bien
---b, mal, ((a,b):xs)
---c, bien
---d, non exhaustive patterns
---e, non exhaustive patterns
---f
+--Bien tipados exhaustivos
+--a: Recibe una dupla con dos elementos de distinto tipo.
+--g: Recibe una funcion (Int -> Int) (que machea con a) y un entero (que machea con b).
 
-f :: [(Int,a)] -> Int
-f [(0,a)] = 3
+--Bien tipados NO exhaustivos
+--c: Recibe una lista de duplas y machea a la primera con x y al resto con xs.
+---- No cubre el caso donde la lista es vacía.
+--d: Recibe una lista de duplas y machea las dos primeras con (x,y) y (a,b) y al resto con xs.
+---- No cubre los casos donde la lista tiene menos de 2 elementos.
+--e: Recibe una lista de duplas con un entero y cualquier elemento no entero.
+---- No cubre los casos donde el entero no es 0.
+--h: Igual que g, pero no es exhaustiva ya que el segundo valor solo puede ser 3.
+
+--Mal tipados
+--b: Intenta machear una lista de tuplas con una única tupla.
+--f: Recibe una lista de duplas con un entero y cualquier elemento no entero
+---- e intenta machear este ultimo con 1 que pertenece a la clase Eq
+---- pero la definicion no filtra con ninguna clase.
+--i: Recibe dos elementos (una función y un entero) e intenta machearlo con 3 elementos.
 
 
 
 --EJERCICIO 14----------------------------------------------
 
---A: se puede y no existe otra porque al no conocer el tipo, no se le pueden aplicar operaciones
+--a: se puede y no existe otra porque al no conocer el tipo, no se le pueden aplicar operaciones
 fA :: (a, b) -> b
 fA (x, y) = y
 
 --El resto no son posibles ya que todas involucran funciones del tipo (a -> b),
---ya sea que el input es de tipo distinto al output o que como parametro tiene una funcion (a -> b)
---No puede existir una funcion que devuelva cualquier otro tipo distinto al inicial
+--ya sea que el input es de tipo distinto al output o que como parametro tiene una funcion (a -> b).
+--No puede existir una funcion que devuelva cualquier otro tipo distinto al inicial.
